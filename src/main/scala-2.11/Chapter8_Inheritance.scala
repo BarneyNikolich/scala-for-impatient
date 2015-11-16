d64eb2257c7d83a236ced45e96d15306869f6530
Barney Nikolich <barney.nikolich@digital.hmrc.gov.uk>
2015-11-16 13:25:40 +0000

    Final comit

--- src/main/scala-2.11/Chapter8_Inheritance.scala
+++ src/main/scala-2.11/Chapter8_Inheritance.scala
@@ -1,3 +1,5 @@
+import javax.management.DescriptorRead
+
 /**
  * Created by barn on 13/11/15.
  */
@@ -76,6 +78,65 @@ object Chapter8_Inheritance extends App{
   bank.deposit(10)
   println(bank)
 
+  /**
+   * 4 - Abstract class Item - defines method types but does not implement them
+   */
+  abstract class Item{
+    def price: Double
+    def description: String
+    override def toString = description + " " + price
+  }
+
+  /**
+   * Sub class of abstract class! Implements the
+   */
+  class SimpleItem(override val price: Double, override val description: String) extends Item
+
+  class Bundle(override val description: String) extends Item {
+    var listOfItems: List[Item] = List() //Creates an empty list of type Item
+    def add(item: Item) = {listOfItems = item :: listOfItems}
+   override  def price = {listOfItems.map(_.price).sum}
+    override def toString = description + "contains: " + listOfItems.map(_.description).mkString(", ") +
+      "Price" + listOfItems.map(_.price).mkString(", ")
+  }
+
+  /**
+   * Create SimpleObjects which together make a Bundle.
+   */
+  val scissors = new SimpleItem(2.00, "Scissors")
+  val plasters = new SimpleItem(1.00, "Plasters")
+  val antiseptic = new SimpleItem(3.00, "Antiseptic cream")
+  val bandage = new SimpleItem(4.00, "Bandages")
+
+  /**
+   * Create the Bundle of items, and add the simple objects into the bundle.
+   */
+  val first_aid_kit = new Bundle("First Aid Kit ")
+  first_aid_kit.add(scissors)
+  first_aid_kit.add(plasters)
+  first_aid_kit.add(antiseptic)
+  first_aid_kit.add(bandage)
+
+  println(first_aid_kit.toString)
+
+
+  class Point(x: Int = 0, y: Int = 0) {
+    override def toString = "["+x + ", " + y + "]"
+  }
+
+  class LabeledPoint(val label: String, x: Int, y: Int) extends Point(x, y) {
+    override def toString = {"Labeled Point = (%s".format(label) + super.toString + ")"}
+
+  }
+
+  object LabeledPoint {
+    def apply(label: String, x: Int, y: Int) = new LabeledPoint(label, x, y)
+  }
+
+  val abcd = new Point(3, 3)
+  println(abcd)
+  val pt = LabeledPoint("Orange Wednesday",44, 44)
+  println(pt)
 
 
 
